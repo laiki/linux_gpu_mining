dc3dc4cf14be40f624120945c4790deb616d2f67
Wasilios Goutas <wasili@goutas.de>
2018-01-29 17:05:33 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,5 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update 
-sudo apt-get install git vim google-chrome-stable
+sudo apt-get install git vim nano 
+#google-chrome-stable
